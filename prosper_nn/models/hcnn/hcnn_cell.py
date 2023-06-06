Fix scaling in Dropout for partial teacher forcing

prosper_nn/models/hcnn/hcnn_cell.py
@@ -21,7 +21,7 @@ Propser_nn is free software: you can redistribute it and/or modify
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-
+from torch import Tensor
 import torch.nn as nn
 import torch
 import torch.nn.utils.prune as prune
@@ -30,6 +30,19 @@ from typing import Optional, Type
 
 def no_dropout_backward(module, grad_in, grad_out):
     return grad_out
+
+
+class PartialTeacherForcing(nn.Dropout):
+    """
+    Applies Dropout as partial teacher forcing. Therefore, scaling of the Dropout is reverted,
+    so that the partial teacher forcing sets the values to the original observation.
+    """
+    def forward(self, input: Tensor) -> Tensor:
+        if not self.training or self.p == 0:
+            return input
+        else:
+            scaled_output = super().forward(input)
+            return (1 - self.p) * scaled_output
 
 
 class HCNNCell(nn.Module):
@@ -109,7 +122,7 @@ class HCNNCell(nn.Module):
         self.eye = torch.eye(
             self.n_features_Y, self.n_state_neurons, requires_grad=False
         )
-        self.ptf_dropout = nn.Dropout(1 - self.teacher_forcing)
+        self.ptf_dropout = PartialTeacherForcing(1 - self.teacher_forcing)
         if self.sparsity > 0:
             prune.random_unstructured(self.A, name="weight", amount=self.sparsity)
         if not self.ptf_in_backward:

prosper_nn/models/hcnn/hcnn_lstm_cell.py
@@ -26,7 +26,7 @@ import torch.nn as nn
 import torch
 import torch.nn.utils.prune as prune
 from typing import Optional, Type
-from .hcnn_cell import no_dropout_backward
+from .hcnn_cell import no_dropout_backward, PartialTeacherForcing
 
 
 class HCNN_LSTM_Cell(nn.Module):
@@ -110,7 +110,7 @@ class HCNN_LSTM_Cell(nn.Module):
         self.eye = torch.eye(
             self.n_features_Y, self.n_state_neurons, requires_grad=False
         )
-        self.ptf_dropout = nn.Dropout(1 - self.teacher_forcing)
+        self.ptf_dropout = PartialTeacherForcing(1 - self.teacher_forcing)
         self.LSTM_regulator = nn.Linear(1, n_state_neurons, bias=False)
         nn.init.ones_(self.LSTM_regulator.weight)
 

prosper_nn/models/hcnn_known_u/hcnn_known_u_cell.py
@@ -26,7 +26,7 @@ import torch.nn as nn
 import torch
 import torch.nn.utils.prune as prune
 from typing import Optional, Type
-from ..hcnn.hcnn_cell import no_dropout_backward
+from ..hcnn.hcnn_cell import no_dropout_backward, PartialTeacherForcing
 
 
 class HCNN_KNOWN_U_Cell(nn.Module):
@@ -139,7 +139,7 @@ class HCNN_KNOWN_U_Cell(nn.Module):
             bias=False,
         )
 
-        self.ptf_dropout = nn.Dropout(1 - self.teacher_forcing)
+        self.ptf_dropout = PartialTeacherForcing(1 - self.teacher_forcing)
         if self.sparsity > 0:
             prune.random_unstructured(self.A, name="weight", amount=self.sparsity)
         if not self.ptf_in_backward: