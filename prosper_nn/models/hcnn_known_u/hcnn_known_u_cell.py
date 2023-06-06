6a91c0b13eba28b9d6110b437c330472e7f93df7
bkn <nico.beck@iis.fraunhofer.de>
2023-06-06 10:38:33 +0200

    Update comment for teacher forcing

--- prosper_nn/models/hcnn_known_u/hcnn_known_u_cell.py
+++ prosper_nn/models/hcnn_known_u/hcnn_known_u_cell.py
@@ -78,7 +78,8 @@ class HCNN_KNOWN_U_Cell(nn.Module):
         teacher_forcing : float
             The probability that teacher forcing is applied for a single state neuron.
             In each time step this is repeated and therefore enforces stochastic learning
-            if the value is smaller than 1.
+            if the value is smaller than 1. Since not all nodes are corrected then, it is
+            partial teacher forcing (ptf).
         backward_full_Y: bool
             Apply partial teacher forcing dropout after or before the output is calculated.
             If True dropout layer is applied afterwards and the output contains the errors of all features.
